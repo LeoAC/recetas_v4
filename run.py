4d16afcbed76249312d129334308f22cc2ce92d7
LeoAC <leonardoaguirre.c@gmail.com>
2024-06-30 23:04:34 -0300

    Entrega final back. Con CORS.Funciona integración.

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from app.views import *
 from app.database import init_app
-#from flask_cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__) 
 init_app(app)
-#CORS(app) 
+CORS(app) 
 
 app.route('/api/recetas/',methods=['GET'])(get_all_recetas) # Obtener todas las peliculas
 app.route('/api/recetas/',methods=['POST'])(create_receta)    # Crear la pelicula
